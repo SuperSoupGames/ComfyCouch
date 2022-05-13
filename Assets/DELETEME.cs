a9b125d5441a7b4037b79b1680fdefb12c8500fa
SuperSoupAdmin <admin@supersoupgames.com>
2022-05-13 00:46:22 -0400

    - Added a backroom  - Alpha character controller added. 60% done importing that.  - Realistic lighting added. Needs work, kinda buggy.

--- Assets/DELETEME.cs
+++ Assets/DELETEME.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 
+/// </summary>
 public class DELETEME : MonoBehaviour
 {
     // Start is called before the first frame update
